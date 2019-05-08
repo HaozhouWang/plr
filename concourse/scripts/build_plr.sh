7e1f6eebac513f2238e7edb667470c4428b3a7cf
Haozhou Wang <hawang@pivotal.io>
2019-05-08 16:08:39 +0800

    fix 23

--- concourse/scripts/build_plr.sh
+++ concourse/scripts/build_plr.sh
@@ -33,8 +33,8 @@ function pkg() {
     source /usr/local/greenplum-db-devel/greenplum_path.sh
 
     ## Install R
-    tar zxf bin_r/bin_r_$OSVER.tar.gz -C /usr/lib64
     if [ "$OSVER" != "ubuntu18" ]; then
+    	tar zxf bin_r/bin_r_$OSVER.tar.gz -C /usr/lib64
         export LD_LIBRARY_PATH=/usr/lib64/R/lib64/R/lib:/usr/lib64/R/lib64/R/extlib:$LD_LIBRARY_PATH
         export R_HOME=/usr/lib64/R/lib64/R
 	export PATH=/usr/lib64/R/bin/:$PATH

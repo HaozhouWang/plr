85bc63c20cba975357a4ad127521cc3740400563
Haozhou Wang <hawang@pivotal.io>
2019-05-08 13:59:52 +0800

    fix 20

--- gppkg/gppkg_rpm.mk
+++ gppkg/gppkg_rpm.mk
@@ -6,7 +6,7 @@ GP_VERSION_NUM := $(GP_MAJORVERSION)
 OS=$(word 1,$(subst _, ,$(BLDARCH)))
 ARCH=$(shell uname -p)
 
-R_RPM_FLAGS="--define 'r_dir $(R_HOME)' --define 'r_ver $(R_VER)' --define 'r_rel $(R_REL)'"" 
+R_RPM_FLAGS="--define 'r_dir $(R_HOME)' --define 'r_ver $(R_VER)' --define 'r_rel $(R_REL)'" 
 R_RPM=R-$(R_VER)-$(R_REL).$(ARCH).rpm
 
 RPM_ARGS=$(subst -, ,$*)
